948f660d22db34d3e3057f5422a680d83053e2d4
aish05-code <84139091+aish05-code@users.noreply.github.com>
2021-10-26 17:28:51 +0530

    Create employee.java

--- employee.java
+++ employee.java
@@ -1,25 +1,25 @@
 import java.util.*;
 class Employee
 {
-    String name,city;
+    String name,address;
     int yoj;
-    public void getInfo(String name, String city, int yoj)
+    public void getInfo(String name, int yoj, String address)
     {
         this.name = name;
-        this.city = city;
-        this.yoj = yoj;          
+        this.yoj = yoj; 
+        this.address = address;
     }
     
     public void printInfo()
     {
-        System.out.println(name  + "\t\t" + city + "\t\t" +yoj );  
+        System.out.println(name + "\t\t" +yoj +"\t\t\t" + address);  
     }
 
     public static void main()
     {
         String name[] = new String[3];
-        String city[]= new String[3];
         int yoj[]= new int[3];
+        String address[]= new String[3];
         for (int i = 0; i <3; i++)
         {  
             Scanner sc= new Scanner (System.in);
@@ -27,18 +27,18 @@ class Employee
             name[i]=sc.nextLine();
             System.out.println("Enter Year of Joining");
             yoj[i]=sc.nextInt();
-            System.out.println("Enter City");
+            System.out.println("Enter address");
             sc.nextLine();
-            city[i]=sc.nextLine();
+            address[i]=sc.nextLine();
         }
         Employee em1 = new Employee(); 
-        em1.getInfo(name[0],city[0], yoj[0]);
+        em1.getInfo(name[0],yoj[0], address[0]);
         Employee em2 = new Employee();
-        em2.getInfo(name[1],city[1], yoj[1]);
+        em2.getInfo(name[1],yoj[1],address[1]);
         Employee em3 = new Employee(); 
-        em3.getInfo(name[2],city[2], yoj[2]);
+        em3.getInfo(name[2],yoj[2], address[2]);
         
-        System.out.println("Name\t\tAddress\t\tYear Of Joining");
+        System.out.println("Name"+"\t\t"+"Year Of Joining"+"\t\t"+"Address");
         em1.printInfo();
         em2.printInfo();
         em3.printInfo();
