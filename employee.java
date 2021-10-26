7c04032cd94ef1fb88ab3e9ee32c470db945a998
aish05-code <84139091+aish05-code@users.noreply.github.com>
2021-10-26 17:31:19 +0530

    Update employee.java

--- employee.java
+++ employee.java
@@ -31,16 +31,15 @@ class Employee
             sc.nextLine();
             address[i]=sc.nextLine();
         }
-        Employee em1 = new Employee(); 
-        em1.getInfo(name[0],yoj[0], address[0]);
-        Employee em2 = new Employee();
-        em2.getInfo(name[1],yoj[1],address[1]);
-        Employee em3 = new Employee(); 
-        em3.getInfo(name[2],yoj[2], address[2]);
-        
+        Employee employ1 = new Employee(); 
+        employ1.getInfo(name[0],yoj[0], address[0]);
+        Employee employ2 = new Employee();
+        employ2.getInfo(name[1],yoj[1],address[1]);
+        Employee employ3 = new Employee(); 
+        employ3.getInfo(name[2],yoj[2], address[2]);
         System.out.println("Name"+"\t\t"+"Year Of Joining"+"\t\t"+"Address");
-        em1.printInfo();
-        em2.printInfo();
-        em3.printInfo();
+        employ1.printInfo();
+        employ2.printInfo();
+        employ3.printInfo();
     }
 }
