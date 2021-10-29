83f1989afd23ddf83018fec62b3a18437f13bd8e
aish05-code <84139091+aish05-code@users.noreply.github.com>
2021-10-29 21:19:30 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Prime
+{
+    int num,count;
+    void input()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println ("Enter a value:");
+        num=sc.nextInt();
+    }
+
+    void check()
+    {
+        for (int i=1;i<=num;i++)
+        {
+            if (num%i==0)
+            {
+                count++;
+            }
+        }
+        if (count==2)
+        {
+            System.out.println (num+"is a Prime Number");
+        }
+        else
+        {
+            System.out.println (num+" is not a Prime Number");
+        }
+    }
+
+    public static void main()
+    {
+        Prime ob =new Prime();
+        ob.input();
+        ob.check();
+    }
+}
